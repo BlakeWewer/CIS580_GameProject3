022761366f57643284f910afb5bd95d2f1aaa9fb
Blake Wewer <bewewer@yahoo.com>
2020-02-12 14:54:33 -0600

    Initial commit

---
+++ MonoGameWindowsStarter/Maze.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+
+namespace MonoGameWindowsStarter
+{
+    class Maze
+    {
+        Game2 game;
+        public List<Wall> walls = new List<Wall>();
+        public List<Spike> spikes = new List<Spike>();
+        public Vector2 startingPosition;
+
+        public Maze(Game2 game)
+        {
+            this.game = game;
+        }
+
+        public void Initialize(List<Tuple<int, int>> wallPositions, List<Tuple<int, int>> spikePositions, Vector2 startingPosition)
+        {
+            for(int i = 0; i < wallPositions.Count(); i++)
+            {
+                Wall wall = new Wall(game);
+                wall.Initialize(new Vector2(wallPositions[i].Item1 * 50,
+                                                wallPositions[i].Item2 * 50),
+                                    false);
+                walls.Add(wall);
+            }
+            for (int i = 0; i < spikePositions.Count(); i++)
+            {
+                Spike spike = new Spike(game);
+                spike.Initialize(new Vector2(spikePositions[i].Item1 * 50,
+                                                spikePositions[i].Item2 * 50));
+                spikes.Add(spike);
+            }
+            this.startingPosition = startingPosition;
+        }
+
+        public void LoadContent()
+        {
+            foreach(Wall wall in walls)
+            {
+                wall.LoadContent();
+            }
+            foreach(Spike spike in spikes)
+            {
+                spike.LoadContent();
+            }
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            foreach(Spike spike in spikes)
+            {
+                spike.Update(gameTime);
+            }
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            foreach(Wall wall in walls)
+            {
+                wall.Draw(spriteBatch);
+            }
+            
+            foreach(Spike spike in spikes)
+            {
+                spike.Draw(spriteBatch);
+            }
+        }
+
+        public void MakeWallBombable(Tuple<int, int> location)
+        {
+            bool found = false;
+            int i = 0;
+            while(!found && i < walls.Count())
+            {
+                if(walls[i].Bounds.X == location.Item1 * 50 
+                    && walls[i].Bounds.Y == location.Item2 * 50)
+                {
+                    walls[i].isBombable = true;
+                    found = true;
+                }
+                i++;
+            }
+        }
+    }
+}
---
+++ MonoGameWindowsStarter/Wall.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+
+namespace MonoGameWindowsStarter
+{
+    class Wall
+    {
+        Game2 game;
+        public BoundingRectangle Bounds;
+        Texture2D texture;
+        public bool isBombable;
+        public bool destroyed;
+
+        //Idea for next project: Invisible/Appearing Walls
+
+        public Wall(Game2 game)
+        {
+            this.game = game;
+        }
+
+        public void Initialize(Vector2 position, bool bombable)
+        {
+            Bounds.Width = 50;
+            Bounds.Height = 50;
+            Bounds.X = position.X;
+            Bounds.Y = position.Y;
+            isBombable = bombable;
+            destroyed = false;
+        }
+
+        public void LoadContent()
+        {
+            if (isBombable)
+                texture = game.Content.Load<Texture2D>("cracked_wall");
+            else
+                texture = game.Content.Load<Texture2D>("wall");
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            if(!destroyed)
+                spriteBatch.Draw(texture, Bounds, Color.White);
+        }
+    }
+}
